f3b399a16693e1067c8a940f9a03f32c51cd0b5a
linspacestrom <ti13murka@mail.ru>
2025-02-05 16:33:15 +0500

    Пока отключил пагинацию и права доступа

--- core/pagination.py
+++ core/pagination.py
@@ -1,58 +1,57 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-
-class UserPageNumberPagination(PageNumberPagination):
-
-    page_size = 10
-    page_size_query_param = "page_size"
-    max_page_size = 100
-
-    def get_paginated_response(self, data):
-        return Response({
-            'count': self.page.paginator.count,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'results': data
-        })
-
-class ProfilePageNumberPagination(PageNumberPagination):
-
-    page_size = 10
-    page_size_query_param = "page_size"
-    max_page_size = 100
-
-    def get_paginated_response(self, data):
-        return Response({
-            'count': self.page.paginator.count,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'results': data
-        })
-
-class AddressPageNumberPagination(PageNumberPagination):
-
-    page_size = 10
-    page_size_query_param = "page_size"
-    max_page_size = 100
-
-    def get_paginated_response(self, data):
-        return Response({
-            'count': self.page.paginator.count,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'results': data
-        })
-
-class EmergencyPageNumberPagination(PageNumberPagination):
-
-    page_size = 10
-    page_size_query_param = "page_size"
-    max_page_size = 100
-
-    def get_paginated_response(self, data):
-        return Response({
-            'count': self.page.paginator.count,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'results': data
-        })
+# class UserPageNumberPagination(PageNumberPagination):
+#
+#     page_size = 10
+#     page_size_query_param = "page_size"
+#     max_page_size = 100
+#
+#     def get_paginated_response(self, data):
+#         return Response({
+#             'count': self.page.paginator.count,
+#             'next': self.get_next_link(),
+#             'previous': self.get_previous_link(),
+#             'results': data
+#         })
+#
+# class ProfilePageNumberPagination(PageNumberPagination):
+#
+#     page_size = 10
+#     page_size_query_param = "page_size"
+#     max_page_size = 100
+#
+#     def get_paginated_response(self, data):
+#         return Response({
+#             'count': self.page.paginator.count,
+#             'next': self.get_next_link(),
+#             'previous': self.get_previous_link(),
+#             'results': data
+#         })
+#
+# class AddressPageNumberPagination(PageNumberPagination):
+#
+#     page_size = 10
+#     page_size_query_param = "page_size"
+#     max_page_size = 100
+#
+#     def get_paginated_response(self, data):
+#         return Response({
+#             'count': self.page.paginator.count,
+#             'next': self.get_next_link(),
+#             'previous': self.get_previous_link(),
+#             'results': data
+#         })
+#
+# class EmergencyPageNumberPagination(PageNumberPagination):
+#
+#     page_size = 10
+#     page_size_query_param = "page_size"
+#     max_page_size = 100
+#
+#     def get_paginated_response(self, data):
+#         return Response({
+#             'count': self.page.paginator.count,
+#             'next': self.get_next_link(),
+#             'previous': self.get_previous_link(),
+#             'results': data
+#         })
